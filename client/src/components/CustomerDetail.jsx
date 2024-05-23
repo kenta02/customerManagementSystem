b7e78ffd92dd97dcc721bb65cfdcbab96150aa89
Kenta Yoshikawa <kenta.yoshikawa.dev@gmail.com>
2024-05-23 12:21:24 +0900

    商談履歴（todoリスト)の追加（フロントエンド機能のみ）

--- client/src/components/CustomerDetail.jsx
+++ client/src/components/CustomerDetail.jsx
@@ -5,8 +5,24 @@ import { useParams } from "react-router-dom";
 
 // 詳細画面
 const CustomerDetailPage = () => {
-  const { id } = useParams();
+  const { id, customer_id } = useParams();
+
+  // 顧客情報の詳細用のステート
   const [customerDetail, setCustomerDetail] = useState(null);
+  // 商談履歴用のステート
+  const [negotiationHistory, setNegotiationHistory] = useState([]);
+
+  //日付用のステート
+  const [selectedDate, setSelectedDate] = useState("");
+
+  //日付用のステート
+  const [negotiationDate, setNegotiationDate] = useState("");
+
+  //商談内容用のステート
+  const [details, setDetails] = useState("");
+
+  // todo用のステート
+  const [todos, setTodos] = useState([]);
 
   // APIから顧客情報を取得する
   useEffect(() => {
@@ -19,7 +35,7 @@ const CustomerDetailPage = () => {
     })
       .then((response) => response.json())
       .then((data) => {
-        console.log(data);
+        // console.log(data);
 
         //　特定のIDに対応するレコードを探す
         const record = data.find((item) => item.id === parseInt(id));
@@ -38,46 +54,143 @@ const CustomerDetailPage = () => {
       );
   }, [id]);
 
+  // APIから商談履歴を取得する
+  useEffect(() => {
+    // APIから商談履歴のデータを取得する
+    const url = `http://localhost:3001/api/get/negotiation_history/`;
+    fetch(url, { method: "GET" })
+      .then((response) => response.json())
+      .then((data) => {
+        console.log(data);
+        console.log("検証開始");
+        // 顧客IDと一致するレコードから日付だけを抽出する
+        const filteredData = data.filter(
+          // 　日付だけ抽出する
+          (item) => item.customer_id === parseInt(id)
+        );
+
+        // 日付の配列を作成する
+        const negotiationDate = filteredData.map((index) => {
+          return index.date;
+        });
+
+        // 日付をUTCからローカルタイムゾーンに変換する
+        const firstDate = new Date(filteredData[0].date);
+        const localDate = new Date(
+          firstDate.getTime() - firstDate.getTimezoneOffset() * 60000
+        );
+        const filteredDate = localDate.toISOString().split("T")[0];
+
+        if (data) {
+          // 見つかったレコードをセットする
+          setNegotiationHistory(data);
+          // 抽出した最初の日付をinputエリアにセットする
+          setSelectedDate(filteredDate);
+        } else {
+          console.log("該当するレコードは見つかりません。");
+        }
+      })
+      .catch((error) => console.log(error));
+  }, [customer_id, id]);
+
   if (!customerDetail) {
     return <>Loading....</>;
   }
+
+  // 追加ボタンがクリックされた時に実行される関数
+  const onClickAdd = () => {
+    console.log("追加ボタン用の関数が実行されました");
+    // inputエリアが空の場合は押しても何もしない。
+    if (!negotiationDate || !details) {
+      return;
+    }
+
+    // 新しい商談履歴オブジェクトを作成する;
+    const newTodos = { negotiationDate, details };
+
+    // スプレッド構文を使って新しい配列を作成する
+    setTodos([...todos, newTodos]);
+
+    // 入力フィールドをクリアする
+    setNegotiationDate("");
+    setDetails("");
+  };
 
   return (
     <>
       <div>
         <h1>詳細画面</h1>
-        <p>{new Date(customerDetail.appointmentDate).toLocaleDateString()}</p>
-        <p>{customerDetail.salesContract}</p>
-        <p>{customerDetail.currentContracts}</p>
-        <p>{customerDetail.companyName}</p>
-        <p>{customerDetail.companyNameKana}</p>
-        <p>¥{new Intl.NumberFormat("ja-JP").format(customerDetail.capital)}</p>
-        <p>{customerDetail.employees}</p>
-        <p>{customerDetail.appointmenpepartment}</p>
-        <p>{customerDetail.representativeName}</p>
-        <p>{customerDetail.representativeNameKana}</p>
+        <p>会社名：{customerDetail.companyName}</p>
+        <p>会社の所在地：{customerDetail.companyAddress}</p>
         <p>
+          資本金：¥
+          {new Intl.NumberFormat("ja-JP").format(customerDetail.capital)}
+        </p>
+        <p>
+          アポ日付：
+          {new Date(customerDetail.appointmentDate).toLocaleDateString()}
+        </p>
+        <p>契約した売上：{customerDetail.salesContract}</p>
+        <p>現在契約本数：{customerDetail.currentContracts}</p>
+        <p>アポ先部署：{customerDetail.appointmentDepartment}</p>
+        <p>担当者名：{customerDetail.representativeName}</p>
+        <p>ふりがな：{customerDetail.representativeNameKana}</p>
+        <p>
+          会社URL：
           <a href={customerDetail.url} target="_blank" rel="noreferrer">
-            URL
+            {customerDetail.url}
           </a>
         </p>
-
         <h2>[商談履歴]</h2>
-
-        <div className="meeting-list">
-          商談日：
-          <input type="date" placeholder="商談日" />
-          <br />
-          商談内容：
-          <input type="text" placeholder="商談内容" />
-          <br />
-          目標数値：
-          <input type="text" placeholder="目標数値" />
-          <br />
-          目標数値と現在数値までいくらか
-          <input type="text" placeholder="目標数値と現在数値までいくらか" />
-        </div>
+        ここには、商談履歴が表示されます。↓
         <br />
+        ※商談履歴はTODOリストの形で追加していく形となります。
+        <br />
+        ※以下はモックデータです。
+        {negotiationHistory.map((history, index) => (
+          <div key={index}>
+            <p>商談日：{new Date(history.date).toLocaleDateString()}</p>
+            <p>商談内容：{history.details}</p>
+          </div>
+        ))}
+        <p>
+          ----------------------------------------------------------------------------------------
+        </p>
+        <p>ここから作成してください↓</p>
+        <div>
+          <div className="input-area">
+            <label htmlFor="negotiation-date">商談日：</label>
+            <input
+              type="date"
+              id="negotiationDate"
+              value={negotiationDate}
+              onChange={(e) => setNegotiationDate(e.target.value)}
+            />
+            <br />
+            <label htmlFor="negotiation-details">商談内容：</label>
+            <input
+              type="text"
+              id="negotiationDetails"
+              value={details}
+              onChange={(e) => setDetails(e.target.value)}
+            />
+            <br />
+            <button onClick={onClickAdd}>追加</button>
+          </div>
+          <p>---------------------------------------------------------</p>
+          <p>[履歴一覧]↓</p>
+
+          <div className="todolist-area">
+            <ul>
+              {todos.map((todo, index) => (
+                <li key={index}>
+                  商談日：{todo.negotiationDate} <br />
+                  商談内容：{todo.details}
+                </li>
+              ))}
+            </ul>
+          </div>
+        </div>
       </div>
     </>
   );
